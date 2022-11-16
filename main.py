3ceaa5f06b65f87d4949c2cc624bd6e949bf2a80
Dan Edström <dan.edstrom@student.kyh.se>
2022-11-16 09:06:50 +0100

    updated main

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ def main():
     for i, station in enumerate(channels, start=1):
         print(i, station["name"])
 
+    choose_channel = input('Choose a channel: ')
+
+    test = int(choose_channel) -1
+    convert = f'0{test}'
+
+    radio_station_id = channels[int(convert)]['id']
+    api_url_channel = f'https://api.sr.se/api/v2/scheduledepisodes/rightnow?format=json&channelid={radio_station_id}'
+    response2 = urllib.request.urlopen(api_url_channel)
+    answer2 = response2.read()
+    json_dict2 = json.loads(answer2)
+
+    print(json_dict2['channel']['currentscheduledepisode']['starttimeutc'])
+    print(json_dict2['channel']['currentscheduledepisode']['program']['name'])
 
 if __name__ == '__main__':
     main()
